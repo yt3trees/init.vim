e634caa50cdb76f751396ad195d2708b55d771bb
yt3trees <y.atforest@gmail.com>
2022-09-16 12:34:44 +0900

    add markdown/highlights.scm

---
+++ after/queries/markdown/highlights.scm
@@ -0,0 +1,11 @@
+[
+  (atx_h1_marker)
+  (atx_h2_marker)
+  (atx_h3_marker)
+  (atx_h4_marker)
+  (atx_h5_marker)
+  (atx_h6_marker)
+  (setext_h1_underline)
+  (setext_h2_underline)
+] @text.title
+
